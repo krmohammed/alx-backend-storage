89c09ce315eb6e2b6dc03ba9aa11a5cc72850a7b
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-08-16 17:37:55 +0000

    task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,29 +8,53 @@ from functools import wraps
 
 def count_calls(method: Callable) -> Callable:
     """count_calls decorator"""
+
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         """wrapper method"""
         key = method.__qualname__
         self._redis.incr(key)
         return method(self, *args, **kwargs)
-    
+
     return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """call_history method"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper method"""
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(output))
+
+        return output
+
+    return wrapper
+
 
 class Cache:
     """Cache class"""
+
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store method"""
         r_key = str(uuid.uuid4())
         self._redis.set(r_key, data)
         return r_key
 
-    def get(self, key: str, fn: Optional[Callable]=None) -> Union[str, bytes, int, float, None]:
+    def get(
+        self, key: str, fn: Optional[Callable] = None
+    ) -> Union[str, bytes, int, float, None]:
         """get method"""
         data = self._redis.get(key)
         if data is None:
